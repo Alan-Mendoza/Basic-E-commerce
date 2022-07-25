02bb706ff138ff0445589f5926f9697253a588ec
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-07-25 15:02:42 -0400

    Creando un layouts master y colocando en orden nuestro layouts para llamar con section y extends

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
     <h1>List of Products</h1>
     @empty($products)
         <div class="alert alert-warning">
@@ -41,5 +34,3 @@
         </table>
     </div>
     @endempty
-</body>
-</html>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') }}</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
     <h1>{{ $product->title }} ({{ $product->id }})</h1>
     <p>{{ $product->description }}</p>
     <p>{{ $product->price }}</p>
@@ -16,5 +9,4 @@
     {{-- {!! $html !!} --}}
     {{-- comentario --}}
     {{-- @{{ $varDeVueJs }} --}}
-</body>
-</html>
+@endsection
