67b28409c4f830a204a62e9197ad0f000dcf38d1
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-07-25 14:20:49 -0400

    Utilizando Modelos y mejorando los query buldiers a modelos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,8 +10,12 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = DB::table('products')->get();
-        dd($products);
+        // $products = DB::table('products')->get();
+        // dd($products);
+            // Modelos
+        $products = Product::all();
+        // return $products;
+
         return view('products.index');
     }
 
@@ -28,8 +33,13 @@ class ProductController extends Controller
     {
         // $product = DB::table('products')->where('id', $product)->get();
         // $product = DB::table('products')->where('id', $product)->first();
-        $product = DB::table('products')->find($product);
-        dd($product);
+        // $product = DB::table('products')->find($product);
+        // dd($product);
+            // Modelos
+        // $product = Product::find($product);
+        $product = Product::findOrFail($product);
+        // return $product;
+
         return view('products.show');
     }
 
