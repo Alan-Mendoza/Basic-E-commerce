273738dcb295e3b2f3e924c1c66eb39e01140697
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-07-25 11:13:09 -0400

    Creando controlador php artisan make:controller MainController y llamando a la ruta welcome

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('main');
+Route::get('/', [MainController::class, 'index'])->name('main');
 
 Route::get('products', [ProductController::class, 'index'])->name('products.index');
 Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
