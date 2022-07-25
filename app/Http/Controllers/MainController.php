b54aad436096145858979ff806006259c17d8735
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-07-25 11:57:47 -0400

    Aprendiendo de errores dd() y dump() para ver errores en nuestro controlador main, dd() detiene la ejecucion y dump()continua con el proceso

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,6 +8,9 @@ class MainController extends Controller
 {
     public function index()
     {
+        // $name = config('app.name');
+        // dump($name);
+        // dd($name);
         return view('welcome');
     }
 }
