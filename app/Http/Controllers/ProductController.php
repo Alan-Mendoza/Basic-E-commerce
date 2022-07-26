12fb4170d8475e93f0aef3231b0f6e5c42f7aa50
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-07-26 19:07:11 -0400

    Aplicando middleweard a nuestro controlador product y viendo las funciones del middlewear con except y only para evitar que rutas queremos que esten protegidas con redireccion al login

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        // $this->middleware('auth')->only('index');
+        // $this->middleware('auth')->except(['index', 'create']);
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         // $products = DB::table('products')->get();
