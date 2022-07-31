d73a407b73877de07c154cbad3461d7e309969da
Alan <alanrodrigo.mg@gmail.com>
2022-07-30 20:37:00 -0400

    Creando la relacion de user a orders por que es de 1 a muchos

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Payment;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,10 +17,16 @@ class Order extends Model
      */
     protected $fillable = [
         'status',
+        'customer_id',
     ];
 
     public function payment()
     {
         return $this->hasOne(Payment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
     }
 }
--- database/migrations/2022_07_27_171145_create_orders_table.php
+++ database/migrations/2022_07_27_171145_create_orders_table.php
@@ -16,8 +16,11 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('status')->default('pending');
+            $table->bigInteger('customer_id')->unsigned();
             // customer_id later
             $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('users');
         });
     }
 
