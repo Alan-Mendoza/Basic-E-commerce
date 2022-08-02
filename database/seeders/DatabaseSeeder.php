d5871986ea8d93bc8bf7769becda37fca86f1ce0
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-08-02 14:48:57 -0400

    Seeders para relaciones normales de 1 a 1 y de 1  a muchos 63

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Payment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +18,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $products = Product::factory(50)->create();
+
+        //RELACIONES NORMALES 1:1 1:N
+        $users = User::factory(20)->create();
+
+        $orders = Order::factory(10)
+                ->make()
+                ->each(function($order) use ($users) {
+                    $order->customer_id = $users->random()->id;
+                    $order->save();
+
+                    $payment = Payment::factory()->make();
+                    // $payment->order_id = $order->id;
+                    // $payment->save();
+                    $order->payment()->save($payment);
+                });
     }
 }
