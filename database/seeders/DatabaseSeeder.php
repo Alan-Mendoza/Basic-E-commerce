1ae6210c06857ad682d3f53f3464411b528d3eea
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-08-02 15:18:04 -0400

    Creando datos en el seeder para las relaciones polimorficas 64

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Card;
+use App\Models\Image;
 use App\Models\Product;
 use App\Models\User;
 use App\Models\Order;
 use App\Models\Payment;
 use Illuminate\Database\Seeder;
+
+use function GuzzleHttp\Promise\each;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,12 +21,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $products = Product::factory(50)->create();
-
         //RELACIONES NORMALES 1:1 1:N
-        $users = User::factory(20)->create();
+        $users = User::factory(20)
+            ->create()
+            ->each(function ($user) {
+                $image = Image::factory()
+                    ->user()
+                    ->make();
+                });
 
-        $orders = Order::factory(10)
+                $orders = Order::factory(10)
                 ->make()
                 ->each(function($order) use ($users) {
                     $order->customer_id = $users->random()->id;
@@ -32,6 +40,27 @@ class DatabaseSeeder extends Seeder
                     // $payment->order_id = $order->id;
                     // $payment->save();
                     $order->payment()->save($payment);
+                });
+
+                $cards = Card::factory(20)->create();
+
+                $products = Product::factory(50)
+                    ->create()
+                    ->each(function ($product) use ($orders, $cards) {
+                        $order = $orders->random();
+
+                        $order->products()->attach([
+                            $product->id => ['quantity' => mt_rand(1, 3)]
+                        ]);
+
+                        $card = $cards->random();
+
+                        $card->products()->attach([
+                            $product->id => ['quantity' => mt_rand(1, 3)],
+                        ]);
+
+                        $images = Image::factory(mt_rand(2, 4))->make();
+                        $product->images()->saveMany($images);
                 });
     }
 }
