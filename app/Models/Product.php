79663fd3545cbe25040fcc41d32835bf00172f06
Alan Mendoza <alanrodrigo.mg@gmail.com>
2022-08-02 12:18:35 -0400

    Relacion de una a mucho con polimorfismo y prueba en tinker, a partir de aca me dejo un rar por que mas adelante borraremos las tablas pibote y dejaremos solo una con un nombre de productables

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Card;
 use App\Models\Order;
+use App\Models\Image;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -32,5 +33,10 @@ class Product extends Model
     public function orders()
     {
         return $this->belongsToMany(Order::class)->withPivot('quantity');
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
     }
 }
